d24bdec12b452c230d49600e2bfd62d88961bcd5
Alexander Šimko <simkoalex@gmail.com>
2024-03-11 10:46:57 +0100

    initial commit

---
+++ Source/CvikaGE2/SpinningActor.cpp
@@ -0,0 +1,77 @@
+#include "SpinningActor.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+ASpinningActor::ASpinningActor() :
+	NumberOfSpins(0),
+	LastYaw(0)
+{
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComponent");
+	SetRootComponent(StaticMeshComponent);
+
+	UStaticMesh *Sphere = LoadObject<UStaticMesh>(nullptr, TEXT("/Engine/BasicShapes/Sphere"));
+	StaticMeshComponent->SetStaticMesh(Sphere);
+
+	
+	SubStaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("SubStaticMeshComponent");
+	SubStaticMeshComponent->SetupAttachment(StaticMeshComponent);
+	SubStaticMeshComponent->SetRelativeLocation(FVector(0,0, 100));
+
+	UStaticMesh *Cube = LoadObject<UStaticMesh>(nullptr, TEXT("/Engine/BasicShapes/Cube"));
+	SubStaticMeshComponent->SetStaticMesh(Cube);
+
+	MovementComponent = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("MovementComponent"));
+
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ASpinningActor::BeginPlay()
+{
+	Super::BeginPlay();
+	LastYaw = GetActorRotation().Yaw;
+	UKismetSystemLibrary::PrintString(this, TEXT("ASpinningActor::BeginPlay()"));
+}
+
+void ASpinningActor::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	
+	double const CurrentYaw = GetActorRotation().Yaw;
+	if (CurrentYaw < LastYaw)
+	{
+		++NumberOfSpins;
+		OnSpin();
+		if (NumberOfSpins == Threshold)
+		{
+			OnThresholdReached();
+		}
+	}
+	LastYaw = CurrentYaw;
+}
+
+// void ASpinningActor::OnSpin()
+// {
+// 	FString const Str = FString::FromInt(NumberOfSpins);
+// 	UKismetSystemLibrary::PrintString(this, Str);
+// }
+
+void ASpinningActor::OnThresholdReached_Implementation()
+{
+	UKismetSystemLibrary::PrintString(this, TEXT("Threshold reached"));
+}
+
+void ASpinningActor::ResetSpinning()
+{
+	NumberOfSpins = 0;
+	LastYaw = GetActorRotation().Yaw;
+}
+
+void ASpinningActor::SetDescription(FString &descrition)
+{
+	
+}
+
+int ASpinningActor::GetNumberOfSpins()
+{
+	return NumberOfSpins;
+}
+
---
+++ Source/CvikaGE2/SpinningActor.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/RotatingMovementComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "SpinningActor.generated.h"
+
+UCLASS(Blueprintable)
+class ASpinningActor : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly,
+		meta=(AllowPrivateAccess="True", DisplayName="My name") )
+	int NumberOfSpins;
+
+	double LastYaw;
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(ExposeOnSpawn="True"))
+	int Threshold;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent *StaticMeshComponent;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent *SubStaticMeshComponent;
+
+	UPROPERTY(EditAnywhere)
+	URotatingMovementComponent *MovementComponent;
+
+	ASpinningActor();
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnSpin();
+
+	UFUNCTION(BlueprintNativeEvent)
+	void OnThresholdReached();
+	
+	UFUNCTION(BlueprintCallable)
+	void ResetSpinning();
+
+	UFUNCTION(BlueprintCallable)
+	void SetDescription( UPARAM(ref) FString &descrition);
+	
+	UFUNCTION(BlueprintCallable)
+	int GetNumberOfSpins();
+};
